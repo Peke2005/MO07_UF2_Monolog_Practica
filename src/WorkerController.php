d7667c3a9dbdfb485bfdc0a3973ae2b6a2cfa2eb
Peke2005 <polcarbajalgarcia@gmail.com>
2024-11-27 20:12:40 +0100

    Comit finish the practica

--- src/WorkerController.php
+++ src/WorkerController.php
@@ -1,7 +1,7 @@
 <?php
 $steps=0;
 // load dependencies
-require './vendor/autoload.php'; 
+require __DIR__ . '/../vendor/autoload.php';
 ++$steps;
 use Monolog\Level;
 use Monolog\Logger;
@@ -10,34 +10,32 @@ use Monolog\Handler\StreamHandler;
 // create log
 $log = new Logger("LogWorkerDB");
 // define logs location
-$log->pushHandler(new StreamHandler("../logs/WorkerDB.log", Level::Error)); 
+$log->pushHandler(new StreamHandler("../logs/WorkerDB.log", Level::Info)); 
 ++$steps;
 
 //ddbb connection, read from miConf.ini
-//TODO
+$db = parse_ini_file("../conf/miConf.ini");
 ++$steps;
-
 try {
     $mysqli = new mysqli($db["host"], $db["user"], $db["pwd"], $db["db_name"]); //4 db
     // write info message with "Connection successfully"
-    //TODO
+    $log->info("Connection successfully");
     ++$steps;
-
     // Create operation
     $sql_sentence = "INSERT INTO worker(dni,name,surname,salary,phone) 
-            VALUES('71111111D','Juan','González',20000,'93500202')";
+            VALUES('71111111B','Juan','González',20000,'93500202')";
 
     try {
         $result = $mysqli->query($sql_sentence);
         // write info message with "Record inserted successfully"
-        //TODO
+        $log->info("Record inserted successfully");
         ++$steps;
     } catch (mysqli_sql_exception $e) {
         //  write error message with "Error inserting a record"
-        //TODO
+        $log->error("Error: inserting a record");
     }
 } catch (mysqli_sql_exception $e) {
     //  write error message with "Error connection db: + details parameters config"
-    //TODO
+    $log->error("Error: connection db: " . $e->getMessage());
 }
 echo "steps executed correctly: " . $steps;
